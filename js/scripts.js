2361b92632383f8d5787e5362707e3e236f6561d
Daniela Codes <daniela.antonio.430@gmail.com>
2023-11-12 18:03:02 -0500

    Updating Objects into Array
    4 - Pokemon's added as Objects with information

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 // Task 1.2 - Pokedex 
 
-let pokemonList = [];  
+ let pokemonList = [
+    {name: 'Bulbasaur', height: 7, type:['grass','poison']},
+    {name: 'Charmander', height: 6, type:'fire'},
+    {name: 'Squirtle', height: 5, type: 'water'},
+    {name: 'Weedle', height: 3, type:['bug','poison']} 
+];  
