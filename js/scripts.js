39731873c51200ddf06a10dd58ae8b8561f4cf68
Daniela Codes <daniela.antonio.430@gmail.com>
2023-11-19 22:40:11 -0500

    FIXED SPACING ISSUE
    ex: Bulbasaur  (Height: 7)
    ***** the SPACE between " " are VERY important.

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,8 @@
 ];  
 
 for (let i=0; i < pokemonList.length; i++ ) {
-    console.log(pokemonList[i].name + "(Height: " , pokemonList[i].height , ")"); 
+    console.log(pokemonList[i].name  + " (Height: " , pokemonList[i].height , ")"); 
 
-    document.write(pokemonList[i].name  + "(Height: " , pokemonList[i].height , ")" );    
+    document.write(pokemonList[i].name + " (Height: " , pokemonList[i].height , ")" );    
     document.write("<br>")
 }
