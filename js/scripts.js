2b2f3b21baf796bc797b7a9a5c91d3db986742ea
Daniela Codes <daniela.antonio.430@gmail.com>
2023-12-09 17:33:31 -0500

    Task_1.5
    -  Wrapped PokemonList array in an IIFE
    -  used add & getAll function to  be able to add a pokemon & also be able to retrieve the list of Pokemons.
    - updated forEach loop using the Repository.

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,54 @@
 // Task 1.2 - Pokedex 
-
- let pokemonList = [
-    {name: 'Bulbasaur', 
-    height: 7, 
-    type:['grass','poison']
-   },
-    {name: 'Charmander',
-     height: 6,
-   type:'fire'
-   },
-    {name: 'Squirtle', 
-    height: 5, 
-    type: 'water'
-   },
-    {name: 'Weedle', 
-    height: 3, 
-    type:['bug','poison']
-   },
-    {name: 'Pikachu', 
-    height: 4, 
-    type:'electric'
-   },
-    {name: 'Snorlax', 
-    height: 2.1,
-     type: 'normal'
-   } 
-
-];  
-   // creating forEach loop 
-
-   pokemonList.forEach(item => document.write(item.name + ' ' + ' (Height: ' + item.height + ')' + ' (Type: ' + item.type + ')' + '<br>'));
-
-
-
-
-// for (let i = 0; i < pokemonList.length; i++) {
-//     if (pokemonList[i].height >4.5) {
-     
-//       // using  document.write 
-
-//       document.write(pokemonList[i].name + " (Height: ", pokemonList[i].height, ") - That is a Big Pokemon!");
-//       document.write("<br>") ;
-  
-//    }else {
-      
-//       document.write(pokemonList[i].name + " (Height: ", pokemonList[i].height, ")");
-//       document.write("<br>") ;
-  
-//    }
-//   }
-
-
+// Wrapping pokemonList array in an IIFE 
+let pokemonRepository = (function() {
+   let pokemonList = [
+     {
+       name: 'Bulbasaur',
+       height: 7,
+       type: ['grass', 'poison']
+     },
+     {
+       name: 'Charmander',
+       height: 6,
+       type: 'fire'
+     },
+     {
+       name: 'Squirtle',
+       height: 5,
+       type: 'water'
+     },
+     {
+       name: 'Weedle',
+       height: 3,
+       type: ['bug', 'poison']
+     },
+     {
+       name: 'Pikachu',
+       height: 4,
+       type: 'electric'
+     },
+     {
+       name: 'Snorlax',
+       height: 2.1,
+       type: 'normal'
+     }
+   ];
+   function add(pokemon) {
+     pokemonList.push(pokemon);
+   }
+ 
+   function getAll() {
+     return pokemonList;
+   }
+ 
+   return {
+     add: add,
+     getAll: getAll
+   };
+ })();
+ 
+ pokemonRepository.getAll().forEach(item => {
+   document.write(item.name + ' (Height: ' + item.height + ')' + ' (Type: ' + item.type + ')' + '<br>');
+ });
+ 
+ 
