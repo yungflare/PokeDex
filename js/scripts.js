530085f3e96b6a289df1e44f76f2198040f1c882
Daniela Codes <daniela.antonio.430@gmail.com>
2024-01-23 21:02:27 -0500

    Task1.10
    Rewrote Code
    arranged for index to match classes in JS
    PokemonModal
    PokemonModalLabel
    
    erased CS code

--- js/scripts.js
+++ js/scripts.js
@@ -5,36 +5,133 @@
     function add(pokemon) {
       pokemonList.push(pokemon);
     }
-
-    function getAll(){
+  
+    function getAll() {
       return pokemonList;
     }
 
-    function showDetails (pokemon) {
+    
+    function showDetails(pokemon) {
       loadDetails(pokemon).then(function () {
         showModal(pokemon.name, pokemon.height);
       });
     }
+  
 
-    function hideModal() { 
-      let modalContainer = document.querySelector("#mo")
-    }
-   
+   function showModal(title, text, img) {
+    let name = document.querySelector("#pokemonModalLabel");
+    let height = document.querySelector("#modal-height");
+    name.innerHTML = "";
+    height.innerHTML= "";
+    name.innerHTML = title;
+    height.innerHTML = `Height: ${text}`;
+   }
+
+
+   function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let newPokemon = document.createElement("li");
+    newPokemon.classList.add("list-group-item")
+    let button = document.createElement("button");
     
+    button.setAttribute("data-toggle", "modal")
+    button.setAttribute("data-target", "#pokemonModal");
+    button.innerText = pokemon.name;
+    button.classList.add("btn", "btn-primary", "btn-lg", "btn-block");
 
+ 
 
+    button.addEventListener("click", function() {
+      showModal(pokemon);
+    });
 
+    newPokemon.append(button);
+    pokemonList.append(newPokemon);
 
+   }
 
+   function showLoadingMessage() {
+    let loadingMessage = document.createElement('p');
+    loadingMessage.textContent = "Loading..."; 
 
+    document.body.appendChild(loadingMessage);
+  }
 
+ // using hideLoadingMessage
 
+ function hideLoadingMessage() {
+  let loadingMessage = document.querySelector('p');
+  if (loadingMessage) {
+    loadingMessage.remove();
+  }
+ }
 
+  // adding loadList function 
 
+  function loadList() {
+    showLoadingMessage();
+    return fetch(apiUrl)
+    .then(function (response) {
+      hideLoadingMessage(); 
+      return response.json(); 
+    })
+    .then(function(json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add (pokemon);
+      });
+    })
+    .catch(function (e) {
+      hideLoadingMessage();
+      console.error(e);
+    });
+    }
 
+    // adding loadDetails function 
 
+    function loadDetails(pokemon) {
+      showLoadingMessage();
+      let url = pokemon.detailsUrl;
+      return fetch(url)
+      .then(function (response) {
+        hideLoadingMessage();
+        return response.json();
+      })
+      .then(function (details) {
+        pokemon.imageUrl = details.sprites.front_default;
+        pokemon.height = details.height;
+        pokemon.types = details.types; 
+      })
+      .catch(function (e) {
+        hideLoadingMessage();
+        console.error(e);
+      });
+      }
 
+// function showDetails(pokemon) {
+// pokemonRepository.loadDetails(pokemon).then(function () {
+//   let typesString = pokemon.types.map(type => type.type.name).join(`,`);
+//  showModal(pokemon.name,`Height: ${pokemon.height} Type: ${typesString}`, pokemon.imageUrl); 
+// });
+// }
 
+ return {
+   add: add,
+   getAll: getAll,
+   addListItem: addListItem,
+   loadList: loadList, 
+   loadDetails: loadDetails,
+   showDetails: showDetails
+ };
+})();
 
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
 
-  });
+});
+  
